fda88112287612ef3c66aa6f1f7d364d27824b52
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-06-25 14:56:22 -0400

    commit tree 0390ce372ae3466f4a988e738e138b39df88a5d5

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+require 'pry'
+
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(badges)
+    badge = []
+    badges.each do |name|
+    badge << "Hello, my name is #{name}."
+    end
+    badge
+end
+
+
+def assign_rooms(room_assignments)
+    assignment = []
+    room_assignments.each_with_index do |name, index|
+    assignment << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    assignment
+end
+
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+    assign_rooms(attendees).each do |assignment|
+        puts assignment
+    end
+end
